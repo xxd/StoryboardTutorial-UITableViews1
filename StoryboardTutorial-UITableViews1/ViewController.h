0edf981aa766f469c9f588ff247d07ccebbf570e
xxd <xxdxxd51@hotmail.com>
2012-03-18 13:07:35 +0800

    done

--- StoryboardTutorial-UITableViews1/ViewController.h
+++ StoryboardTutorial-UITableViews1/ViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DetailViewController.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDelegate,UITableViewDataSource> {
+
+}
+@property(nonatomic,retain) NSMutableDictionary *states;
+@property(nonatomic,retain) NSArray *datasource;
+
+-(void)setupArray;
 
 @end
--- StoryboardTutorial-UITableViews1/ViewController.m
+++ StoryboardTutorial-UITableViews1/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#import "DetailViewController.h"
 @implementation ViewController
-
+@synthesize states,datasource;
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -20,6 +20,7 @@
 
 - (void)viewDidLoad
 {
+    [self setupArray];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -29,6 +30,63 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+}
+-(void)setupArray{
+    
+    states = [[NSMutableDictionary alloc]init];
+    [states setObject:@"Lansing" forKey:@"Michigan"];
+    [states setObject:@"Sacremento" forKey:@"California"];
+    [states setObject:@"Albany" forKey:@"New York"];
+    [states setObject:@"Phoenix" forKey:@"Arizona"];
+    [states setObject:@"Tulsa" forKey:@"Oklahoma"];
+    
+    datasource = [states allKeys];
+    
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = (UITableViewCell *) [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    //---------- CELL BACKGROUND IMAGE -----------------------------
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:cell.frame];
+    UIImage *image = [UIImage imageNamed:@"LightGrey@2x.png"];
+    imageView.image = image;
+    cell.backgroundView = imageView;
+    [[cell textLabel] setBackgroundColor:[UIColor clearColor]];
+    [[cell detailTextLabel] setBackgroundColor:[UIColor clearColor]];
+    
+    cell.textLabel.text = [datasource objectAtIndex:indexPath.row];
+    
+    //Arrow 
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    DetailViewController *detail = [self.storyboard instantiateViewControllerWithIdentifier:@"detail"];
+    detail.state = [datasource objectAtIndex:indexPath.row];
+    detail.captial = [states objectForKey:detail.state];
+    [self.navigationController pushViewController:detail animated:YES];
+    
+}
+
+//------------------TableView Cell Height------------------
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 60;
 }
 
 - (void)viewWillAppear:(BOOL)animated
